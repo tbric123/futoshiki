e79558df5a543359b9f80c4d8971b40e621b95d8
Tom Bricknell <t.bricknell93@gmail.com>
2025-11-15 17:45:38 +1000

    Start initialising the grid

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("Welcome to Futoshiki!");
+
+function initialiseGrid() {
+  const grid = document.querySelector(".grid");
+
+  for (let i = 0; i < 6; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < 6; j++) {
+      const gridSquare = document.createElement("span");
+      gridSquare.classList.add("gridSquare");
+      gridSquare.innerHTML = "0";
+      row.appendChild(gridSquare);
+    }
+
+    grid.appendChild(row);
+  }
+}
+
+initialiseGrid();
